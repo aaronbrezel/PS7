cea031908ec93d9e3d79769f02bf8fc9f2dd3905
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-04-26 12:22:24 -0500

    PS7

--- PS7.R
+++ PS7.R
@@ -76,7 +76,97 @@ robberyCrimeByDistrict <- cleanCrimeStats %>%
                           summarise(count = n()) %>%
                           mutate(count, propotionOfTotalCrime = count/crimeByDistrict$count[2:7])
 
+#for question three I am going to divide crime by its crime code. For example a crime code in the 250,000 means loitering
 
-unique(cleanCrimeStats$cleanDescpt) #Of the 29 seeminly unique crimes, "ROBBERY," "ROBBERY CARJACKING,"
-#"BURGLARY," "LARCENY," "AUTO THEFT," "STLG BY DECEIT," "FAILURE TO RETURN BORROWED," 
-#"EMBEZZLEMENT" and "STOLEN PROPERTY" are what I would consider "crime related to robbery" 
+class(cleanCrimeStats$Crime[1]) #Crime column is class integer. Can use logicals to seperate out
+
+#reclassifies rows for ggplot
+cleanCrimeStatsGGPLOT <- cleanCrimeStats %>%
+                    mutate(Crime, generalCrime = sapply(Crime, function(x){ #Simplifies all the different types of assaults to just "ASSAULT". I know the grepl pattern is funny
+                      if(x == 10000){
+                        x = "HOMICIDE"
+                      }
+                      else if (x >= 20000 & x < 30000){
+                        x = "RAPE"
+                      }
+                      else if (x >= 30000 & x < 40000){
+                        x = "ROBBERY"
+                      }
+                      else if (x >= 40000 & x < 50000){
+                        x = "ASSAULT"
+                      }
+                      else if (x >= 50000 & x < 60000){
+                        x = "BURGLARY"
+                      }
+                      else if (x >= 60000 & x < 70000){
+                        x = "LARCENY"
+                      }
+                      else if (x >= 70000 & x < 80000){
+                        x = "AUTO THEFT"
+                      }
+                      else if (x >= 80000 & x < 90000){
+                        x = "ARSON"
+                      }
+                      else if (x >= 90000 & x < 100000){
+                        x = "ASSAULT"
+                      }
+                      else if (x >= 100000 & x < 110000){
+                        x = "FORGERY"
+                      }
+                      else if (x >= 110000 & x < 120000){
+                        x = "FRAUD/IDENTITY THEFT"
+                      }
+                      else if (x >= 120000 & x < 130000){
+                        x = "EMBEZZLEMENT"
+                      }
+                      else if (x >= 130000 & x < 140000){
+                        x = "STOLEN PROPERTY"
+                      }
+                      else if (x >= 140000 & x < 150000){
+                        x = "DESTRUCTION OF PROPERTY"
+                      }
+                      else if (x >= 150000 & x < 160000){
+                        x = "WEAPONS CHARGE"
+                      }
+                      else if (x >= 170000 & x < 180000){
+                        x = "SEX OFFENSE/ABUSE"
+                      }
+                      else if (x >= 180000 & x < 190000){
+                        x = "DRUG POSSESSION "
+                      }
+                      else if (x >= 200000 & x < 210000){
+                        x = "FAMILY/CHILD ENDANGERMENT"
+                      }
+                      else if (x >= 210000 & x < 220000){
+                        x = "DUI"
+                      }
+                      else if (x >= 220000 & x < 230000){
+                        x = "LIQOUR LAWS"
+                      }
+                      else if (x >= 240000 & x < 250000){
+                        x = "DISORDERLY CONDUCT"
+                      }
+                      else if (x >= 250000 & x < 260000){
+                        x = "LOITERING"
+                      }
+                      else if (x >= 260000 & x < 270000){
+                        x = "MISC VIOLATIONS"
+                      }
+                    }))
+
+cleanCrimeStatsGGPLOTDay <- cleanCrimeStatsGGPLOT%>%
+                            group_by(cleanDay, generalCrime) %>%
+                            summarise(count = n()) %>%
+                            arrange(generalCrime) 
+
+#temp
+#ggplot(cleanCrimeStatsGGPLOTDay, aes(x = cleanCrimeStatsGGPLOTDay$cleanDay, y = cleanCrimeStatsGGPLOTDay$count, colour = cleanCrimeStatsGGPLOTDay$generalCrime)) + geom_point() + xlab("Day") + ylab("Number of incidents") + ggtitle("St. Louis March Crime") + labs(color = "Type of Crime")
+
+ggplot(cleanCrimeStatsGGPLOTDay, aes(x = cleanDay, y = count, colour = generalCrime)) + geom_point() + xlab("Day") + ylab("Number of incidents") + ggtitle("St. Louis March Crime") + labs(color = "Type of Crime")
+
+cleanCrimeStatsGGPLOTDistrict <- cleanCrimeStatsGGPLOT %>% 
+                                  group_by(District, cleanDay) %>%
+                                  summarise(count = n())
+
+ggplot(cleanCrimeStatsGGPLOTDistrict, aes(x = cleanCrimeStatsGGPLOTDistrict$cleanDay, y = cleanCrimeStatsGGPLOTDistrict$count, colour = cleanCrimeStatsGGPLOTDistrict$District)) + geom_line() + xlab("Day") + ylab("Number of incidents") + ggtitle("St. Louis March Crime") + labs(color = "Type of Crime")
+
